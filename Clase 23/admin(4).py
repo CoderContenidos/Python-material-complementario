a0c2e50aa45c827c203f2901fe5c218f514048c9
Coderhouse <89412239+CoderContenidos@users.noreply.github.com>
2023-06-02 14:02:47 -0300

    Add files via upload

---
+++ Clase 23/admin(4).py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from  .models import * #importamos el archivo models
+
+# Register your models here.
+#registramos los modelos
+
+admin.site.register(Curso)
+
+admin.site.register(Estudiante)
+
+admin.site.register(Profesor)
+
+admin.site.register(Entregable)
